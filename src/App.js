ce1f68355f3d8b5b55a3ed4cfa5909ff9734570c
Jorge Barrueta <jbarrueta99@hotmail.com>
2021-05-06 19:55:31 -0500

    Proof of Concept

--- src/App.js
+++ src/App.js
@@ -1,124 +1,124 @@
-import React, { Component, Fragment } from 'react';
-import Particles from 'react-particles-js';
-import Clarifai from 'clarifai'
-import Signin from './components/Signin/Signin';
-import Register from './components/Register/Register';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import Rank from './components/Rank/Rank';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import Particles from "react-particles-js";
+import Clarifai from "clarifai";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Rank from "./components/Rank/Rank";
+import "./App.css";
 
 const particlesOptions = {
-  "particles": {
-    "number": {
-      "value": 80,
-      "density": {
-        "enable": true,
-        "value_area": 700
-      }
-    },
-    "color": {
-      "value": "#ffffff"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 700,
       },
     },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 0.1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
+    color: {
+      value: "#ffffff",
     },
-    "size": {
-      "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 10,
-        "size_min": 0.1,
-        "sync": false
-      }
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
     },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 0.1,
+        opacity_min: 0.1,
+        sync: false,
+      },
     },
-    "move": {
-      "enable": true,
-      "speed": 2,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 10,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
   },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "grab"
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "grab",
       },
-      "onclick": {
-        "enable": true,
-        "mode": "push"
+      onclick: {
+        enable: true,
+        mode: "push",
       },
-      "resize": true
+      resize: true,
     },
-    "modes": {
-      "grab": {
-        "distance": 140,
-        "line_linked": {
-          "opacity": 1
-        }
+    modes: {
+      grab: {
+        distance: 140,
+        line_linked: {
+          opacity: 1,
+        },
       },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 3,
       },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
+      repulse: {
+        distance: 200,
+        duration: 0.4,
       },
-      "push": {
-        "particles_nb": 4
+      push: {
+        particles_nb: 4,
       },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
+      remove: {
+        particles_nb: 2,
+      },
+    },
   },
-  "retina_detect": true
-}
+  retina_detect: true,
+};
 
 const app = new Clarifai.App({
-  apiKey: 'b501e1101b054bc3bf9ec78153232cd0'
-})
+  apiKey: "b501e1101b054bc3bf9ec78153232cd0",
+});
 
 //{const url = "https://samples.clarifai.com/metro-north.jpg"}
 
@@ -126,89 +126,120 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: '',
-      imageUrl: '',
+      input: "",
+      imageUrl: "",
       box: {},
-      route: 'signin',
-      isSignedIn: false
-    }
+      route: "signin",
+      isSignedIn: false,
+      user: {
+        id: "",
+        name: "",
+        email: "",
+        entries: 0,
+        joined: "",
+      },
+    };
   }
 
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
+
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputImage');
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputImage");
     const width = Number(image.width);
     const height = Number(image.height);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
     };
-  }
+  };
 
   displayFaceBox = (box) => {
-    this.setState({ box: box })
-  }
+    this.setState({ box: box });
+  };
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
-    ;
-  }
+  };
 
   onSubmit = () => {
     this.setState({ imageUrl: this.state.input });
     app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL,
-        this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-      .catch(err => console.log(err));
-  }
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) => {
+        if (response) {
+          console.log(this.state.user.id);
+          fetch("/image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: this.state.user.id,
+            }),
+          })
+            .then((response) => response.json())
+            .then((count) => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
+            });
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
+      .catch((err) => console.log(err));
+  };
 
   onRouteChange = (route) => {
     this.setState({
-      isSignedIn: route === 'signout' ?
-        false :
-        route === 'home' ?
-          true :
-          false,
-      route: route
-    })
-  }
+      isSignedIn: route === "signout" ? false : route === "home" ? true : false,
+      route: route,
+    });
+  };
 
   render() {
-    const { isSignedIn, imageUrl, route, box } = this.state
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-        <Particles className='particles'
-          params={particlesOptions} />
-        <Navigation onRouteChange={this.onRouteChange}
-          isSignedIn={isSignedIn} />
-        {route === 'home' ?
+        {/* <Particles className='particles'
+          params={particlesOptions} /> */}
+        <Navigation
+          onRouteChange={this.onRouteChange}
+          isSignedIn={isSignedIn}
+        />
+        {route === "home" ? (
           <Fragment>
             <Logo />
-            <Rank />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
             <ImageLinkForm
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onSubmit}
             />
-            <FaceRecognition
-              imageUrl={imageUrl}
-              box={box} />
-          </Fragment> : (
-            route === 'signin' ?
-              <Signin onRouteChange={this.onRouteChange} /> :
-              <Register onRouteChange={this.onRouteChange} />
-          )
-
-
-        }
-
+            <FaceRecognition imageUrl={imageUrl} box={box} />
+          </Fragment>
+        ) : route === "signin" ? (
+          <Signin onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
+        ) : (
+          <Register
+            onRouteChange={this.onRouteChange}
+            loadUser={this.loadUser}
+          />
+        )}
       </div>
     );
   }
-
 }
 
 export default App;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,38 +1,105 @@
-import React from 'react';
+import React, { Component } from "react";
 
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      name: "",
+    };
+  }
 
-const Register = ({ onRouteChange }) => {
+  onEmailChange = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  onPasswordChange = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  onNameChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  onSubmitSignIn = () => {
+    fetch("/register", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password,
+        name: this.state.name,
+      }),
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        if (user) {
+          this.props.loadUser(user);
+          this.props.onRouteChange("home");
+        }
+      });
+  };
+
+  render() {
     return (
-        <div>            
-            <article className="br3 ba white b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-3 center">
-                <main className="pa4 white-80">
-                    <div className="measure">
-                        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                            <legend className="f1 fw6 ph0 mh0">Register</legend>
-                            <div className="mv3">
-                                <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
-                                <input className="pa2 input-reset ba b--white bg-transparent hover-bg-white hover-white w-100" type="text" name="name" id="name" />
-                            </div>
-                            <div className="mv3">
-                                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                                <input className="pa2 input-reset ba b--white bg-transparent hover-bg-white hover-white w-100" type="email" name="email-address" id="email-address" />
-                            </div>
-                            <div className="mv3">
-                                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                                <input className="b pa2 input-reset ba b--white bg-transparent hover-bg-white hover-white w-100" type="password" name="password" id="password" />
-                            </div>
-                        </fieldset>
-                        <div className="">
-                            <input className="b ph3 pv2 input-reset ba white b--white bg-transparent grow pointer f5 dib" 
-                            type="submit" 
-                            value="Register" 
-                            onClick={() => onRouteChange('home')} />
-                        </div>
-                    </div>
-                </main>
-            </article>
-        </div>
-    )
+      <div>
+        <article className="br3 ba bw3 white b--white-20 mv4 w-100 w-50-m w-25-l mw6 shadow-3 center">
+          <main className="pa4 white-80">
+            <div className="measure">
+              <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                <legend className="f1 fw6 ph0 mh0">Register</legend>
+                <div className="mv3">
+                  <label className="db fw6 lh-copy f6" htmlFor="name">
+                    Name
+                  </label>
+                  <input
+                    className="pa2 input-reset ba black b--white bg-white-50 hover-bg-white w-100"
+                    type="text"
+                    name="name"
+                    id="name"
+                    onChange={this.onNameChange}
+                  />
+                </div>
+                <div className="mv3">
+                  <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                    Email
+                  </label>
+                  <input
+                    className="pa2 input-reset ba black b--white bg-white-50 hover-bg-white w-100"
+                    type="email"
+                    name="email-address"
+                    id="email-address"
+                    onChange={this.onEmailChange}
+                  />
+                </div>
+                <div className="mv3">
+                  <label className="db fw6 lh-copy f6" htmlFor="password">
+                    Password
+                  </label>
+                  <input
+                    className="b pa2 input-reset ba black b--white bg-white-50 hover-bg-white w-100"
+                    type="password"
+                    name="password"
+                    id="password"
+                    onChange={this.onPasswordChange}
+                  />
+                </div>
+              </fieldset>
+              <div className="">
+                <input
+                  className="b ph3 pv2 input-reset ba white b--white bg-transparent grow pointer f5 dib"
+                  type="submit"
+                  value="Register"
+                  onClick={this.onSubmitSignIn}
+                />
+              </div>
+            </div>
+          </main>
+        </article>
+      </div>
+    );
+  }
 }
 
 export default Register;
